ac3134bd9a37b750808017c238207f5ff8eaebf8
p-schaefer <patrick.schaefer@mail.utoronto.ca>
2017-10-14 18:04:32 -0400

    habitat vs biological distance

--- Testing Scripts/for publication2.R
+++ Testing Scripts/for publication2.R
@@ -1,3 +1,26 @@
+xpand <- function(d) do.call("expand.grid", rep(list(1:nrow(d)), 2))
+euc_norm <- function(x) sqrt(sum(x^2))
+euc_dist <- function(mat, weights=1) {
+  iter <- xpand(mat)
+  vec <- mapply(function(i,j) euc_norm(weights*(mat[i,] - mat[j,])), 
+                iter[,1], iter[,2])
+  matrix(vec,nrow(mat), nrow(mat))
+}
+
+format.pval(pv = pvalues, 
+            
+            # digits : number of digits, but after the 0.0
+            digits = 2, 
+            
+            # eps = the threshold value above wich the 
+            # function will replace the pvalue by "<0.0xxx"
+            eps = 0.001, 
+            
+            # nsmall = how much tails 0 to keep if digits of 
+            # original value < to digits defined
+            nsmall = 3
+)
+
 #load datasets
 data(YKEnvData,envir = environment()) # Environmental dataset
 data(YKBioData,envir = environment()) # Biological dataset
@@ -404,3 +427,129 @@ plot(1:30,dist.dif[1:30])
 points(1:30,(1*(1/((3:32)^(2)))))
 points(1:30,exp(-(1:30)))
 #distance<-dist(test, method = "euclidean")
+
+##########################################################################################
+#habitat and bio distance
+##########################################################################################
+
+
+yk1<-bio.data.test$Raw.Data
+yk1<-yk1[1:158,]
+#yk1<-yk1[,apply(yk1,2,function(x)length(which(x>1)))>5]
+#yk1<-yk1[,colSums(yk1)>5]
+
+#M<-data.frame(cor(cbind(yk1,yk2)))
+M<-(cor(cbind(yk1,yk2),method="spearman"))
+M1<-as.data.frame(as.table(M))
+M1<-M1[M1$Var1%in%colnames(yk1),]
+M1<-M1[M1$Var2%in%colnames(yk2),]
+
+M2<-M1[order(abs(M1$Freq),decreasing = T),]
+M2<-M2[abs(M2$Freq)>0.3,]
+unique(M2$Var2)
+
+#yk2<-YKEnvData[,colnames(YKEnvData)%in%as.character(unique(M2$Var2))]
+yk2<-YKEnvData
+yk2<-scale(yk2[1:158,])
+habitat.dist1<-dist(yk2)
+habitat.dist2.1<-vegan::rda(yk2,scale=T)
+habitat.dist2<-habitat.dist2.1$CA$u[,1:4]
+habitat.dist2<-decostand(habitat.dist2,method="range")
+habitat.dist2<-as.dist(euc_dist(habitat.dist2,weights=(eigenvals(habitat.dist2.1)/sum(eigenvals(habitat.dist2.1)))[1:4]))
+
+#habitat.dist2<-habitat.dist2*(eigenvals(habitat.dist2.1)[1:ncol(habitat.dist2)]/sum(eigenvals(habitat.dist2.1)[1:ncol(habitat.dist2)]))
+#habitat.dist2<-dist(habitat.dist2) 
+ 
+
+M<-M[1:ncol(yk1),(ncol(yk1)+1):ncol(M)]
+corrplot::corrplot(M, method = "number")
+
+bio.dist1<-dist(decostand(yk1,method="hellinger"))
+bio.dist1.1<-vegan::rda(decostand(yk1,method="hellinger"),scale=T)
+bio.dist1<-bio.dist1.1$CA$u[,1:4]
+bio.dist1<-decostand(bio.dist1,method="range")
+bio.dist1<-as.dist(euc_dist(bio.dist1,weights=(eigenvals(bio.dist1.1)/sum(eigenvals(bio.dist1.1)))[1:4]))
+
+bio.dist2<-vegan::vegdist(yk1)
+
+lm1<-lm(dist(habitat.dist2.1$CA$u[,1:2])~dist(bio.dist1.1$CA$u[,1:2]))
+pro1<-protest(habitat.dist2.1,bio.dist1.1,permutations=10000)
+man1<-mantel(bio.dist2,habitat.dist2,method="pearson", permutations = 9999)
+
+
+#plot(protest(bio.dist1.1,habitat.dist2.1))
+smoothScatter(dist(bio.dist1.1$CA$u[,1:2]),dist(habitat.dist2.1$CA$u[,1:2]),cex=0.1,col="grey",main="YK",xlab="Biological Distance",ylab="Habitat Distance")
+abline(lm1)
+text(0.4,0.55,bquote(R^2==.(round(man1$statistic, 2))))
+text(0.4,0.6,paste0("p = ",(round(pro1$signif, 3))))
+
+
+
+summary(lm(bio.dist1~habitat.dist2))
+summary(lm(bio.dist2~habitat.dist2))
+
+summary(lm(bio.dist1~habitat.dist1))
+summary(lm(bio.dist2~habitat.dist1))
+
+smoothScatter(habitat.dist2,bio.dist2)
+scatter.smooth(habitat.dist2,bio.dist2,degree=2,cex=0.1,col="grey")
+
+##########################################################################################
+#habitat and bio distance1
+##########################################################################################
+#data(c("YKBioData","GLBioData","ACTBioData","YKEnvData","GLEnvData","ACTEnvData"))
+GLEnvData<-GLEnvData[-c(grep("1216",rownames(GLEnvData))),]
+GLBioData<-GLBioData[-c(grep("1216",GLBioData$V1)),]
+GLEnvData<-GLEnvData[,-c(21)]
+
+bio.data<-list(YKBioData,GLBioData,ACTBioData)
+env.data<-list(YKEnvData,GLEnvData,ACTEnvData)
+plot.main<-c("YK","GL","ACT")
+
+
+
+for (i in 1:3){
+  bio.data.test<-BenthicAnalysis::benth.met(bio.data[[i]],2,2)
+
+  yk1<-bio.data.test$Raw.Data
+#site.max<-max(grep("D0",rownames(yk1)))
+  
+  yk1<-yk1[grep("D0|T",rownames(yk1)),]
+  yk1<-yk1[,apply(yk1,2,function(x)length(which(x>1)))>5]
+  yk1<-yk1[,colSums(yk1)>5]
+  
+  bio.dist1<-dist(decostand(yk1,method="hellinger"))
+  bio.dist1.1<-vegan::rda(decostand(yk1,method="hellinger"),scale=T)
+  bio.dist1<-bio.dist1.1$CA$u[,1:4]
+  bio.dist1<-decostand(bio.dist1,method="range")
+  bio.dist1<-as.dist(euc_dist(bio.dist1,weights=(eigenvals(bio.dist1.1)/sum(eigenvals(bio.dist1.1)))[1:4]))
+  bio.dist2<-vegan::vegdist(yk1)
+
+  yk2<-env.data[[i]]
+  yk2<-scale(yk2[grep("D0|T",rownames(yk2)),])
+  habitat.dist1<-dist(yk2)
+  habitat.dist2.1<-vegan::rda(yk2,scale=T)
+  habitat.dist2<-habitat.dist2.1$CA$u[,1:4]
+  habitat.dist2<-decostand(habitat.dist2,method="range")
+  habitat.dist2<-as.dist(euc_dist(habitat.dist2,weights=(eigenvals(habitat.dist2.1)/sum(eigenvals(habitat.dist2.1)))[1:4]))
+  
+  lm1<-lm(dist(habitat.dist2.1$CA$u[,1:2])~dist(bio.dist1.1$CA$u[,1:2]))
+  #pro1<-protest(habitat.dist2.1,bio.dist1.1,permutations=10000)
+  man1<-mantel(dist(bio.dist1.1$CA$u[,1:2]),dist(habitat.dist2.1$CA$u[,1:2]),method="pearson", permutations = 9999)
+  
+  x.max<-max(dist(bio.dist1.1$CA$u[,1:2]))
+  y.max<-max(dist(habitat.dist2.1$CA$u[,1:2]))
+  
+  #plot(protest(bio.dist1.1,habitat.dist2.1))
+  smoothScatter(dist(bio.dist1.1$CA$u[,1:2]),dist(habitat.dist2.1$CA$u[,1:2])
+                ,cex=0.1,col="grey",main=plot.main[i],xlab="Biological Distance",ylab="Habitat Distance")
+  abline(lm1)
+  text(x.max-0.1,y.max-0.05,bquote(R^2==.(round(man1$statistic, 3))))
+  text(x.max-0.1,y.max-0.11,paste0("p = ",format.pval(pv = man1$signif,
+                                                      digits = 2, 
+                                                      eps = 0.001,
+                                                      nsmall = 3
+  )))
+
+}
+
